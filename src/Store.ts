8964c228819ed342f2b423bb447996e9d7eb2001
Adam Zielonka <root@adamzielonka.pro>
2023-04-19 09:53:55 +0200

    Store info in url

--- src/Store.ts
+++ src/Store.ts
@@ -1,10 +1,9 @@
-import { makeAutoObservable } from 'mobx'
+import { makeAutoObservable, reaction } from 'mobx'
 
 export class CounterStore {
-  value = 0
-
-  constructor() {
+  constructor(public value = 0) {
     makeAutoObservable(this)
+    this.value = value
   }
 
   addOne = (): void => {
@@ -48,6 +47,14 @@ export class Store {
     return this.counters.reduce((p, c) => p + c.value, 0)
   }
 
+  get toString(): string {
+    return this.counters.map(c => c.value).join(';')
+  }
+
+  fromString(value: string): void {
+    this.counters = value.split(';').map(v => !isNaN(+v) ? new CounterStore(+v) : new CounterStore())
+  }
+
   addCounter = (): void => {
     if (this.canAddMoreCounters) this.counters.push(new CounterStore())
   }
@@ -58,6 +65,14 @@ export class Store {
 }
 
 export const store = new Store()
+const urlParams = new URLSearchParams(window.location.search)
+store.fromString(urlParams.get('value') || '')
+
+reaction(() => store.toString, (value) => {
+  const urlParams = new URLSearchParams(window.location.search)
+  urlParams.set('value', value)
+  window.history.replaceState({}, '', `?${urlParams.toString()}`)
+})
 
 declare global { interface Window { store: Store; }}
 window.store = store
